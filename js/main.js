bc74c57ff92c2138ee4e438a394558d40a078fc3
Marlon070996Ford <--help>
2019-03-27 16:17:01 +0200

    working on my website

--- js/main.js
+++ js/main.js
@@ -12,3 +12,31 @@ $('img[data-enlargable]').addClass('img-enlargable').click(function(){
         $(this).remove();
     }).appendTo('body');
 });
+// function Item(name, price) {
+//     this.name = name;
+//     this.price = "R" + price;
+
+// }
+
+// Item.prototype.description = function () {
+//     var description = "This is a beautiful " + this.name + " for the low price of " + this.price + "."
+//      return description;
+// }
+
+// let bed = new Item("bed", 800);
+// let couch = new Item("couch", 500);
+// let chair = new Item("chair", 300);
+// let table = new Item("table", 400);
+
+
+// // bed item on shop page
+
+// let description = document.getElementById("item-1-describe");
+//     description.innerHTML = bed.description();
+
+//     price = document.getElementById("item-1-price");
+//     price.innerHTML = bed.price;
+
+
+
+// // Couch item on shop page
