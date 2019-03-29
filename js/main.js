bed5ab1ebd363462349743b1755d414735756f8b
Marlon070996Ford <--help>
2019-03-29 15:23:17 +0200

    working on my website

--- js/main.js
+++ js/main.js
@@ -9,19 +9,19 @@ Item.prototype.description = function () {
      return description;
 }
 
-let bed = new Item("bed", 800);
-let couch = new Item("couch", 500);
-let chair = new Item("chair", 300);
-let table = new Item("table", 400);
+let chair = new Item("chair", 800);
+let table = new Item("table", 1000);
+let bed = new Item("bed", 2300);
+let couch = new Item("couch", 1500);
 
 
-// bed item on shop page
+// chair item on shop page
 
 let description = document.getElementById("item-1-describe");
-    description.innerHTML = bed.description();
+    description.innerHTML = chair.description();
 
     price = document.getElementById("item-1-price");
-    price.innerHTML = bed.price;
+    price.innerHTML = chair.price;
 
 
 
@@ -29,7 +29,26 @@ let description = document.getElementById("item-1-describe");
 
 
     description = document.getElementById("item-2-describe");
-    description.innerHTML = couch.description();
+    description.innerHTML = table.description();
 
     price = document.getElementById("item-2-price");
+    price.innerHTML = table.price;
+
+
+// bed item shop page
+
+    description = document.getElementById("item-3-describe");
+    description.innerHTML = bed.description();
+    
+    price = document.getElementById("item-3-describe");
+    price.innerHTML = bed.price;
+
+
+// table item on shop page
+
+    description = document.getElementById("item-4-describe");
+    description.innerHTML = couch.description();
+
+    price = document.getElementById("item-4-describe");
     price.innerHTML = couch.price;
+
